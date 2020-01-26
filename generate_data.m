c6ec7615a94b78d5de41d934c448efcf688f4abd
Krikamol Muandet <krikamol@gmail.com>
2020-01-26 14:05:35 +0100

    update the data generating process

--- generate_data.m
+++ generate_data.m
@@ -1,7 +1,50 @@
-function [ output_args ] = generate_data( input_args )
+function [Z,X] = generate_data(dgp,delta,n)
 %GENERATE_DATA generates data according to the specified DGP.
-%   Detailed explanation goes here
+%   
+%   INPUT: 
+%       dgp - the data generating process ('linreg_hom','linreg_het','simeq')
+%       delta - the parameter controlling the deviation from the null
+%       n - sample size
+%
+%   OUTPUT: 
+%       Z,X - the data matrices
+%
 
+rng('default') % For reproducibility
+
+if strcmp(dgp,'linreg_hom') || strcmp(dgp,'linreg_het')
+    % 
+    theta_1 = 1; 
+    theta_2 = 2;
+    inv_mill_ratio = @(a) normpdf(a)./normcdf(a);
+    
+    X = normrnd(0,1,[n,1]);
+    
+    err = normrnd(0,1,[n,1]); % homoskedastic noise
+    if strcmp(dgp,'linreg_het')
+        err = err.*sqrt(0.1 + 0.1.*(X.^2)); % heteroskedastic noise
+    end
+    
+    Y = theta_1 + theta_2.*X + delta.*inv_mill_ratio((theta_1 + theta_2.*X)./delta) + err;
+    Z = [Y,X];
+    
+elseif strcmp(dgp,'simeq')
+    %
+    alpha_d = -1;
+    beta_d = 1;
+    alpha_s = 1;
+    beta_2 = 1;
+    
+    
+    
+    
+    Z = [Q,P,R,W];
+    X = [R,W];
+    
+else
+    % no specified dgp 
+    error('KCM: The specified DGP is unavailable.');
+end
 
 end
 
--- kcm.m
+++ kcm.m
@@ -1,4 +1,4 @@
-function [mh,gm,dec] = kcm(Z,X,gres,theta,kern,bsize,alpha)
+function [dec,mh,gm,bvals] = kcm(Z,X,gres,theta,kern,bsize,alpha)
 %KCM: The kernel conditional moment (KCM) test. 
 %   
 %   INPUT:  Z - vector of observations
@@ -9,29 +9,31 @@ function [mh,gm,dec] = kcm(Z,X,gres,theta,kern,bsize,alpha)
 %           bsize - bootstrap sample size
 %           alpha - significance level
 %
-%   OUTPUT: mh - value of the test statistic
+%   OUTPUT: dec - result of hypothesis testing (1: reject, 0: fail to reject)
+%           mh - value of the test statistic
 %           gm - (1-alpha)-quantile of the bootstrap statistics
-%           dec - result of hypothesis testing (1: reject, 0: fail to reject)
+%           bvals - a vector of bootstrap statistics
 %
 
 % check the compatibility of data dimensions
 n = size(Z,1);
 if n ~= size(X,1)
-    % exit with a warning message
+    % exit with an error message
+    error("KCM: Z and X must have the same number of rows.");
 end
 
-% evaluate kernel matrix
+% calculate the median distance of X and evaluate the kernel k
 sx = median_inter(X);
 K = kern(X,X,sx);
 
-% evaluate the kernel h 
+% evaluate the generalized residuals and the kernel h_theta
 G = gres(Z,theta);
 H = (G*G').*K;
 
-% evaluate the test statistic
+% calculate the KCM test statistic
 mh = sum(sum(H))/(n*n) - sum(diag(H))/n;
 
-% bootstrapping
+% approximate critical values via bootstrapping 
 bvals = zeros(1,bsize);
 for b=1:bsize
     % draw multinomial random samples
@@ -41,15 +43,14 @@ for b=1:bsize
     WH = (w'*w).*H;
     mhs = sum(sum(WH)) - sum(diag(WH));
     
-    % record the value
+    % record the bootstrap value
     bvals(b) = n*mhs;
-    
 end
 
-% calculate the (1-\alpha)-quantile of the recorded statistics
-gm = quantile(bval,1.-alpha);
+% calculate the (1-alpha)-quantile of the bootstrap statistics
+gm = quantile(bvals,1.-alpha);
 
-% perform the test
+% conduct the test
 dec = 0;
 if gm < n*Mh
     dec = 1;
