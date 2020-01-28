41d7c5203d2a5e56259cf7e4ac4364d9409f14f9
Krikamol Muandet <krikamol@tuebingen.mpg.de>
2020-01-28 13:44:27 +0100

    update code for regression setting

--- run_experiments.m
+++ run_experiments.m
@@ -7,13 +7,21 @@ addpath('./utils')
 rng(9) % For reproducibility
 
 alpha = 0.05;
-bsize = 500;
+bsize = 1000;
+num_repeats = 500;
 
 %% Experiment 1
 
 dgp = "linreg_hom";
-ns = [50,100,200,300];
+ns = [100,200,300,400,500,600,700,800,900,1000];
 delta = 0.05;
-num_repeats = 200;
+
+multiple_experiments(dgp,ns,delta,num_repeats,bsize,alpha);
+
+%% Experiment 2
+
+dgp = "linreg_het";
+ns = [50,100,200,300,400,500,1000];
+delta = 0.05;
 
 multiple_experiments(dgp,ns,delta,num_repeats,bsize,alpha);
