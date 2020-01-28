f1591e33b878cd169821a360bbb652f9c3b47420
Krikamol Muandet <krikamol@gmail.com>
2020-01-29 00:12:07 +0100

    try out the simeq setting

--- run_experiments.m
+++ run_experiments.m
@@ -7,9 +7,9 @@ addpath('./utils')
 rng(9) % For reproducibility
 
 alpha = 0.05;
-delta = 0.05;
-bsize = 1000;
-num_repeats = 500;
+delta = 0.0001;
+bsize = 100;
+num_repeats = 100;
 ns = [100,200,300,400,500,600,700,800,900,1000];
 
 %% Experiment 1: Linear Regression with Homogeneous Error
@@ -25,4 +25,5 @@ multiple_experiments(dgp,ns,delta,num_repeats,bsize,alpha);
 %% Experiment 3: Simultaneous Equation Model
 
 dgp = "simeq";
+ns = [10,20,30,40,50,60,70,80,100];
 multiple_experiments(dgp,ns,delta,num_repeats,bsize,alpha);
--- utils/median_inter.m
+++ utils/median_inter.m
@@ -2,14 +2,14 @@ function [ mdx ] = median_inter(X)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
-n = size(X,1);
-A = repmat(X,1,n);
-B = A';
-dist = abs(A-B);
-dist = reshape(dist,[],1);
-mdx = median(dist);
+%n = size(X,1);
+%A = repmat(X,1,n);
+%B = A';
+%dist = abs(A-B);
+%dist = reshape(dist,[],1);
+%mdx = median(dist);
 
-%mdx = median(squareform(squaredist(X)));
+mdx = median(squareform(squaredist(X')));
 
 end
 
