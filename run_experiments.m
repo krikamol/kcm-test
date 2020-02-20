59f5165d6f790eab9267ca6807b61ba6392c14cc
Krikamol Muandet <krikamol@gmail.com>
2020-02-20 21:17:08 +0100

    update code

--- run_experiments.m
+++ run_experiments.m
@@ -11,10 +11,11 @@ rng(9) % For reproducibility
 
 dim = 5;
 alpha = 0.05;
-delta = 0;
+delta = 0.01;
 bsize = 1000;
-num_trials = 2000;
+num_trials = 300;
 ns = [20,50,100,200,500,1000];
+%ns = [20,50,100,500,1000];
 
 tests = {'kcm','icm','smooth'};
 
