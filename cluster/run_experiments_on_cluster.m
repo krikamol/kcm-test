847f34d6c85d63ad3ee24bcb00ce7664002efab4
Krikamol Muandet <krikamol@tuebingen.mpg.de>
2020-06-10 09:58:25 +0200

    update code for cluster

--- cluster/run_experiments_on_cluster.m
+++ cluster/run_experiments_on_cluster.m
@@ -5,16 +5,28 @@ addpath('../utils')
 
 % initialization
 
-rng(9) % For reproducibility
+rng(trial_no) % For reproducibility
 
-dim = 5;
 alpha = 0.05;
-delta = 0.01;
 bsize = 1000;
-ns = [100,200,400,600,800,1000];
 tests = {'kcm','icm','smooth'};
+if strcmp(experiment_name,'samplesize') 
+    dim = 5;
+    delta = 0.01;
+    ns = [100,200,400,600,800,1000];
+elseif strcmp(experiment_name,'delta')
+    dim = 5;
+    delta = [0.001, 0.01, 0.05, 0.1];
+    ns = 500;
+elseif strcmp(experiment_name,'error')
+    dim = 5;
+    alpha = 0.05;
+    delta = 0.01;
+    ns = [100,200,400,600,800,1000];
+end
 
-outfile = strcat('results/',dgp,'_',experiment_name,'_',int2str(trial_no+1),'.mat');
+outdir = strcat('results/',dgp,'/',experiment_name,'/');
+outfile = strcat(outdir,dgp,'_',experiment_name,'_',int2str(trial_no+1),'.mat');
 
 % experiment
 multiple_experiments_on_cluster(dgp,tests,ns,dim,delta,1,bsize,alpha,outfile);
