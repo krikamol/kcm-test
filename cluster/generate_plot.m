193bef00b8175193ba01e541b85d4c77018053e1
Krikamol Muandet <krikamol@tuebingen.mpg.de>
2020-06-17 12:17:44 +0200

    add gitignore file

--- cluster/generate_plot.m
+++ cluster/generate_plot.m
@@ -4,7 +4,7 @@ addpath('../')
 addpath('../utils')
 
 dgp = 'simeq';
-experiment_name = 'error';
+experiment_name = 'delta';
 
 dim = 5;
 alpha = 0.05;
@@ -16,8 +16,10 @@ if strcmp(experiment_name,'samplesize')
     ns = [100,200,400,600,800,1000];
 elseif strcmp(experiment_name,'delta')
     dim = 5;
-    delta = [0.001,0.005,0.01,0.05,0.1,0.15];
-    ns = 100;
+    %delta = [0.001,0.005,0.01,0.05,0.1,0.15];
+    %ns = 100;
+    delta = linspace(1e-4,1e-2,6);
+    ns = 500;
 elseif strcmp(experiment_name,'error')
     dim = 5;
     alpha = 0.05;
